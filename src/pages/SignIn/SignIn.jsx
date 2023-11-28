1cc40468b99fbeb6b01103eeeda802953f44c8f3
Ben <benviscosi@gmail.com>
2023-11-27 19:23:23 -0500

    sign in page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.css';
+import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
-	return <div>SignIn</div>;
+	let navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const handleSignIn = async () => {};
+
+	const handleKeyPress = (e) => {
+		if (e.key === 'Enter') {
+			handleSignIn();
+		}
+	};
+
+	return (
+		<div className='signin-container'>
+			<form className='signin-form'>
+				<h2>Sign In</h2>
+				<input
+					type='email'
+					placeholder='Email'
+					onChange={(e) => setEmail(e.target.value)}
+					onKeyPress={handleKeyPress} // Added keypress event handler
+				/>
+
+				<input
+					type='password'
+					placeholder='Password'
+					onChange={(e) => setPassword(e.target.value)}
+					onKeyPress={handleKeyPress} // Added keypress event handler
+				/>
+
+				{errorMessage && <div className='error-message'>{errorMessage}</div>}
+
+				<button onClick={handleSignIn}>Sign In</button>
+			</form>
+		</div>
+	);
 };
 
 export default SignIn;
