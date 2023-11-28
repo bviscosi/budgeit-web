429642848a6bf8dbcb21af209c2312e072e60f21
Ben <benviscosi@gmail.com>
2023-11-27 19:19:06 -0500

    basic pagination and architecture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import RoutesComponent from './navigation/RoutesComponent';
 
 function App() {
-	return <div className='App'>BudgeIt</div>;
+	return (
+		<div className='App'>
+			<RoutesComponent />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/navigation/RoutesComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import SignIn from '../pages/SignIn/SignIn';
+import SignUp from '../pages/SignUp/SignUp';
+
+const RoutesComponent = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/sign-in' element={<SignIn />} />
+			<Route path='/sign-up' element={<SignUp />} />
+		</Routes>
+	);
+};
+
+export default RoutesComponent;
