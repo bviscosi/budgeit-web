efb45c295feb65c7db72071cfbb5fae1f6e23bb1
Ben <benviscosi@gmail.com>
2023-11-28 23:48:47 -0500

    styles

--- frontend/src/pages/Home/components/Sidebar/Sidebar.jsx
+++ frontend/src/pages/Home/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import b_logo from '../../../../assets/b.png';
 import HomeIcon from '@mui/icons-material/Home';
 import SettingsIcon from '@mui/icons-material/Settings';
@@ -10,8 +10,17 @@ import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import { Button } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import Person from '@mui/icons-material/Person';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+	const navigate = useNavigate();
+	const [active, setActive] = useState('settings');
+
+	const handleSetActive = (page) => {
+		setActive(page);
+		// navigate('/' + page);
+	};
+
 	return (
 		<div className='sidebar'>
 			<img
@@ -28,21 +37,61 @@ const Sidebar = () => {
 					gap: '7vh',
 					justifyContent: 'center',
 				}}>
-				<Button sx={{ color: '#6c7793' }}>
+				<Button
+					sx={{
+						color: '#d8dee8',
+						backgroundColor: active === 'dashboard' ? '#14141f' : 'transparent',
+						'&:hover': {
+							backgroundColor: active === 'dashboard' ? '#14141f' : 'transparent',
+						},
+					}}
+					onClick={() => handleSetActive('dashboard')}>
 					<GridViewIcon />
 				</Button>
-				<Button sx={{ color: '#6c7793' }}>
+				<Button
+					sx={{
+						color: '#d8dee8',
+						backgroundColor: active === 'savings' ? '#14141f' : 'transparent',
+						'&:hover': {
+							backgroundColor: active === 'savings' ? '#14141f' : 'transparent',
+						},
+					}}
+					onClick={() => handleSetActive('savings')}>
 					<SavingsIcon />
 				</Button>
-				<Button sx={{ color: '#6c7793' }}>
+				<Button
+					sx={{
+						color: '#d8dee8',
+						backgroundColor: active === 'wallet' ? '#14141f' : 'transparent',
+						'&:hover': {
+							backgroundColor: active === 'wallet' ? '#14141f' : 'transparent',
+						},
+					}}
+					onClick={() => handleSetActive('wallet')}>
 					<WalletIcon />
 				</Button>
-				<Button sx={{ color: '#6c7793' }}>
+				<Button
+					sx={{
+						color: '#d8dee8',
+						backgroundColor: active === 'account' ? '#14141f' : 'transparent',
+						'&:hover': {
+							backgroundColor: active === 'account' ? '#14141f' : 'transparent',
+						},
+					}}
+					onClick={() => handleSetActive('account')}>
 					<PersonIcon />
 				</Button>
 			</div>
 
-			<Button sx={{ color: '#6c7793' }}>
+			<Button
+				sx={{
+					color: '#d8dee8',
+					backgroundColor: active === 'settings' ? '#14141f' : 'transparent',
+					'&:hover': {
+						backgroundColor: active === 'settings' ? '#14141f' : 'transparent',
+					},
+				}}
+				onClick={() => handleSetActive('settings')}>
 				<SettingsIcon />
 			</Button>
 		</div>
