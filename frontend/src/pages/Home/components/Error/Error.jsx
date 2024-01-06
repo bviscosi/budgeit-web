995259b805a79865881ef8d5ccc61b85f9da1c97
Ben <benviscosi@gmail.com>
2024-01-06 16:48:48 -0500

    show Error if get transactions fails

--- frontend/src/pages/Home/components/Error/Error.jsx
+++ frontend/src/pages/Home/components/Error/Error.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Error.css'; // Assuming you will create a separate CSS file
 
 const Error = () => {
-	return <div>Error</div>;
+	return (
+		<div className='error-container'>
+			<div className='error-icon'>⚠️</div> {/* Unicode Warning Sign */}
+			<div className='error-message'>An error occurred. Please try again later.</div>
+		</div>
+	);
 };
 
 export default Error;
