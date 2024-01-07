8a57b06233a8cb47438b832fb336daaf9067c34b
Ben <benviscosi@gmail.com>
2024-01-07 12:43:22 -0500

    add return to sign in button on error page

--- frontend/src/pages/Home/components/Error/Error.jsx
+++ frontend/src/pages/Home/components/Error/Error.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Error.css'; // Assuming you will create a separate CSS file
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
+	const navigate = useNavigate();
 	return (
 		<div className='error-container'>
 			<div className='error-icon'>⚠️</div> {/* Unicode Warning Sign */}
 			<div className='error-message'>An error occurred. Please try again later.</div>
+			<Button
+				variant='contained'
+				sx={{ marginTop: '5rem' }}
+				onClick={() => {
+					navigate('/sign-in');
+				}}>
+				Return to Sign In
+			</Button>
 		</div>
 	);
 };
