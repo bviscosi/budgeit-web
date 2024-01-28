0eb0a9adddd1324ae1f11c5f45c3f98473491f52
Ben <benviscosi@gmail.com>
2024-01-28 14:58:30 -0500

    extract styles to styles.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import { Box, Grid, Paper, Typography } from '@mui/material';
 import axios from 'axios';
 import Loading from './components/Loading/Loading';
 import Error from './components/Error/Error';
-import { home } from './styles';
+import { body, grid, home, paper } from './styles';
 import NetWorth from './components/NetWorth/NetWorth';
 import Budgets from './components/Budgets/Budgets';
 import { useCustomTheme } from '../../context/ThemeContext';
@@ -56,23 +56,10 @@ const Home = ({ handleLogout }) => {
 			{!error && !loading && (
 				<div style={home}>
 					<Sidebar tab={tab} setTab={setTab} />
-					<div
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-							width: '100%',
-							height: '100%',
-						}}>
+					<div style={body}>
 						<Topbar handleLogout={handleLogout}></Topbar>
 
-						<Grid
-							container
-							spacing={{ xs: 2, sm: 2, md: 3 }}
-							sx={{
-								height: '100%',
-								width: '100%',
-								padding: '2rem',
-							}}>
+						<Grid container spacing={{ xs: 2, sm: 2, md: 3 }} sx={grid}>
 							<Grid item xs={6} md={6}>
 								<NetWorth />
 							</Grid>
@@ -80,18 +67,7 @@ const Home = ({ handleLogout }) => {
 								<Budgets />
 							</Grid>
 							<Grid item xs={6} md={6}>
-								<Paper
-									style={{
-										display: 'flex',
-										height: '100%',
-										width: '100%',
-										// padding: '10px',
-										// border: `1px solid ${theme.palette.card.border}`,
-										boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.05)',
-
-										// backgroundColor: '#f7f8fa',
-										borderRadius: '20px',
-									}}>
+								<Paper style={paper}>
 									<Typography
 										variant='h3'
 										fontSize='24px'
@@ -102,17 +78,7 @@ const Home = ({ handleLogout }) => {
 								</Paper>
 							</Grid>
 							<Grid item xs={6} md={6}>
-								<Paper
-									style={{
-										display: 'flex',
-										height: '100%',
-										width: '100%',
-										// border: `1px solid ${theme.palette.card.border}`,
-										boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.05)',
-
-										// backgroundColor: '#f7f8fa',
-										borderRadius: '20px',
-									}}></Paper>
+								<Paper style={paper}></Paper>
 							</Grid>
 						</Grid>
 					</div>
--- frontend/src/pages/Home/styles.jsx
+++ frontend/src/pages/Home/styles.jsx
@@ -4,3 +4,28 @@ export const home = {
 	width: '100%',
 	height: '100%',
 };
+
+export const body = {
+	display: 'flex',
+	flexDirection: 'column',
+	width: '100%',
+	height: '100%',
+};
+
+export const grid = {
+	height: '100%',
+	width: '100%',
+	padding: '2rem',
+};
+
+export const paper = {
+	display: 'flex',
+	height: '100%',
+	width: '100%',
+	// padding: '10px',
+	// border: `1px solid ${theme.palette.card.border}`,
+	boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.05)',
+
+	// backgroundColor: '#f7f8fa',
+	borderRadius: '20px',
+};
