d49bc9a4ce8c7cd17dca8d3963ae3b2caeb0b5b5
Ben <benviscosi@gmail.com>
2024-03-20 19:37:15 -0400

    refactor dropdown menu

--- frontend/src/pages/Dashboard/components/Topbar/UserDropdownMenu/UserDropdownMenu.jsx
+++ frontend/src/pages/Dashboard/components/Topbar/UserDropdownMenu/UserDropdownMenu.jsx
@@ -3,6 +3,7 @@ import { Button, Typography, Menu, MenuItem, styled } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { useNavigate } from 'react-router-dom';
+import { button, menuItem } from './styles';
 
 // Custom style for the Menu
 const StyledMenu = styled(Menu)(({ theme }) => ({
@@ -33,21 +34,7 @@ const UserDropdownMenu = ({ handleLogout }) => {
 		<>
 			<Button
 				ref={buttonRef} // Attach the ref to the button
-				sx={{
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'space-evenly', // This will ensure spacing around the name and icon
-					border: '1px solid #474747',
-					borderRadius: '30px',
-					boxShadow: '0px 4px 8px rgba(0.5, 0.5, 0.5, 0.1)',
-					textTransform: 'none',
-					color: 'white',
-					padding: '0 1rem 0 1rem',
-					width: '200px', // Specify the width to match the menu width
-					'&:hover': {
-						backgroundColor: 'rgba(255, 255, 255, 0.08)',
-					},
-				}}
+				sx={button}
 				onClick={handleClick}
 				endIcon={open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}>
 				<Typography variant='p' component='span' sx={{ textAlign: 'center' }}>
@@ -71,7 +58,7 @@ const UserDropdownMenu = ({ handleLogout }) => {
 						handleLogout();
 						navigate('/sign-in');
 					}}
-					sx={{ color: 'white', '&:hover': { backgroundColor: 'rgba(255, 255, 255, 0.2)' } }}>
+					sx={menuItem}>
 					Logout
 				</MenuItem>
 			</StyledMenu>
--- frontend/src/pages/Dashboard/components/Topbar/UserDropdownMenu/styles.jsx
+++ frontend/src/pages/Dashboard/components/Topbar/UserDropdownMenu/styles.jsx
@@ -11,3 +11,24 @@ export const profileCircle = {
 	boxShadow: '0px 4px 8px rgba(0.5, 0.5, 0.5, 0.1)',
 	// backgroundColor: '#F7F8FA',
 };
+
+export const button = {
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'space-evenly', // This will ensure spacing around the name and icon
+	border: '1px solid #474747',
+	borderRadius: '30px',
+	boxShadow: '0px 4px 8px rgba(0.5, 0.5, 0.5, 0.1)',
+	textTransform: 'none',
+	color: 'white',
+	padding: '0 1rem 0 1rem',
+	width: '200px', // Specify the width to match the menu width
+	'&:hover': {
+		backgroundColor: 'rgba(255, 255, 255, 0.08)',
+	},
+};
+
+export const menuItem = {
+	color: 'white',
+	'&:hover': { backgroundColor: 'rgba(255, 255, 255, 0.2)' },
+};
