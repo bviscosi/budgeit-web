ed6c4d2ab377d8411af9ac066bd98a6e952f1594
Ben <benviscosi@gmail.com>
2024-03-23 16:01:52 -0400

    naviagte to sign in on click of get started

--- frontend/src/components/Buttons/GetStartedButton.jsx
+++ frontend/src/components/Buttons/GetStartedButton.jsx
@@ -1,10 +1,18 @@
 import { Button } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const GetStartedButton = ({ sx = {}, ...props }) => {
+	const navigate = useNavigate();
 	return (
-		<Button variant='contained' sx={{ borderRadius: 4, ...sx }} {...props}>
+		<Button
+			variant='contained'
+			sx={{ borderRadius: 4, ...sx }}
+			{...props}
+			onClick={() => {
+				navigate('/sign-in');
+			}}>
 			Get Started
 			<KeyboardArrowRightIcon />
 		</Button>
