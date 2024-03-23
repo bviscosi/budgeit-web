564a0896016989631245e546cc7aead0f6857b17
Ben <benviscosi@gmail.com>
2024-03-23 13:35:32 -0400

    remove css styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import RoutesComponent from './utils/navigation/RoutesComponent';
 
 function App() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeContextProvider } from './utils/theme/ThemeContext';
+import { CssBaseline } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<ThemeContextProvider>
 			<BrowserRouter>
+				<CssBaseline />
 				<App />
 			</BrowserRouter>
 		</ThemeContextProvider>
