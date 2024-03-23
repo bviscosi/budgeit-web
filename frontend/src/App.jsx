2f9ff6501ce64a3d6be19963e2224afe5c271232
Ben <benviscosi@gmail.com>
2024-03-23 13:32:16 -0400

    move theme and browser router providers to main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import './App.css';
 import RoutesComponent from './utils/navigation/RoutesComponent';
-import { ThemeContextProvider } from './utils/theme/ThemeContext';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
-		<ThemeContextProvider>
-			<BrowserRouter>
-				<div className='App'>
-					<RoutesComponent />
-				</div>
-			</BrowserRouter>
-		</ThemeContextProvider>
+		<div className='App'>
+			<RoutesComponent />
+		</div>
 	);
 }
 
