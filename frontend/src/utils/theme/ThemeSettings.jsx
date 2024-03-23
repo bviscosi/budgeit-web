2626fb849e386f05b85621a4dc3abe1a4bd0b105
Ben <benviscosi@gmail.com>
2024-03-23 13:56:29 -0400

    split theme into separate files for better dev experience

---
+++ frontend/src/utils/theme/ThemeSettings.jsx
@@ -0,0 +1,42 @@
+import typography from './Typography';
+
+const themeSettings = {
+	light: {
+		palette: {
+			mode: 'light',
+			primary: { main: '#3f51b5' },
+			secondary: { main: '#f50057' },
+			accent: { main: '#000' },
+			gray: { 100: '#e3e3e3' },
+			card: { border: '#e6e8ec' },
+			background: {
+				paper: '#f7f8fa',
+				main: '#eff1f5',
+				purpleGradient: 'linear-gradient(#242acf, #b57bee)',
+			},
+		},
+		typography,
+	},
+	dark: {
+		palette: {
+			mode: 'dark',
+			primary: { main: '#6573c3' },
+			secondary: { main: '#ff4081' },
+			// Define other colors for dark mode as needed
+			card: { border: '#424242' },
+			gray: { 1: '#bdbdbd', 2: '#424242' },
+
+			background: {
+				default: '#000',
+				paper: '#111415',
+				main: '#0c0e0f',
+				// purpleGradient: 'linear-gradient(#242acf, #b57bee)',
+				purpleGradient:
+					'linear-gradient(60deg, rgba(2,10,18,1) 0%, rgba(82,50,135,1) 70%, rgba(0,90,116,0.8) 100%)',
+			},
+		},
+		typography,
+	},
+};
+
+export default themeSettings;
