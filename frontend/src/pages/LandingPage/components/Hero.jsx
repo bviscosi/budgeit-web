fccd9fe5d78a3e36dd8a57d221db7a43844c14f6
Ben <benviscosi@gmail.com>
2024-03-23 15:19:03 -0400

    Learn more button, responsive stack for buttons

--- frontend/src/pages/LandingPage/components/Hero.jsx
+++ frontend/src/pages/LandingPage/components/Hero.jsx
@@ -2,6 +2,13 @@ import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import React from 'react';
 import GetStartedButton from '../../../components/Buttons/GetStartedButton';
 
+const LearnMoreButton = () => {
+	return (
+		<Button variant='outlined' sx={{ borderRadius: 4, height: 58, px: 3 }}>
+			Learn More
+		</Button>
+	);
+};
 const Hero = () => {
 	return (
 		<Box>
@@ -10,8 +17,9 @@ const Hero = () => {
 					<Typography variant='h1'>Save smarter with BudgeIt</Typography>
 					{/* <Typography variant='h2'>Smart tools for smarter savings</Typography> */}
 					<Typography variant='h2'>Effortless budgeting, simplified savings.</Typography>
-					<Stack direction='row'>
+					<Stack direction={{ xs: 'column', md: 'row' }} alignItems='center' spacing={4}>
 						<GetStartedButton sx={{ height: 58, px: 3 }} />
+						<LearnMoreButton />
 					</Stack>
 				</Stack>
 			</Container>
