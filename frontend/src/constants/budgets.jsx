660216f4b6f4f0aed33404c54a081893ebeb73e3
Ben <benviscosi@gmail.com>
2024-03-31 22:40:25 -0400

    pull budgets array to constants folder

---
+++ frontend/src/constants/budgets.jsx
@@ -0,0 +1,42 @@
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import FlightIcon from '@mui/icons-material/Flight';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+
+export const budgets = [
+	{
+		title: 'Food & Restaurants',
+		amount: '$412.00',
+		icon: <FastfoodIcon />,
+		color: '#34c5c5', // Predefined MUI color
+	},
+	{
+		title: 'Travel',
+		amount: '$123.00',
+		icon: <FlightIcon />,
+		color: '#347cc5', // Predefined MUI color
+	},
+	{
+		title: 'Credit Card',
+		amount: '$12,003.00',
+		icon: <CreditCardIcon />,
+		color: '#3434c5', // Predefined MUI color
+	},
+	{
+		title: 'Food & Restaurants',
+		amount: '$412.00',
+		icon: <FastfoodIcon />,
+		color: '#7c34c5', // Predefined MUI color
+	},
+	{
+		title: 'Travel',
+		amount: '$123.00',
+		icon: <FlightIcon />,
+		color: '#c534c5', // Predefined MUI color
+	},
+	{
+		title: 'Credit Card',
+		amount: '$12,003.00',
+		icon: <CreditCardIcon />,
+		color: '#347cc5', // Predefined MUI color
+	},
+];
--- frontend/src/pages/Dashboard/tabs/Home/components/Budgets.jsx
+++ frontend/src/pages/Dashboard/tabs/Home/components/Budgets.jsx
@@ -1,47 +1,6 @@
 import React from 'react';
 import { Card, CardContent, Typography, Box, Divider, CircularProgress } from '@mui/material';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import FlightIcon from '@mui/icons-material/Flight';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-
-const budgets = [
-	{
-		title: 'Food & Restaurants',
-		amount: '$412.00',
-		icon: <FastfoodIcon />,
-		color: '#34c5c5', // Predefined MUI color
-	},
-	{
-		title: 'Travel',
-		amount: '$123.00',
-		icon: <FlightIcon />,
-		color: '#347cc5', // Predefined MUI color
-	},
-	{
-		title: 'Credit Card',
-		amount: '$12,003.00',
-		icon: <CreditCardIcon />,
-		color: '#3434c5', // Predefined MUI color
-	},
-	{
-		title: 'Food & Restaurants',
-		amount: '$412.00',
-		icon: <FastfoodIcon />,
-		color: '#7c34c5', // Predefined MUI color
-	},
-	{
-		title: 'Travel',
-		amount: '$123.00',
-		icon: <FlightIcon />,
-		color: '#c534c5', // Predefined MUI color
-	},
-	{
-		title: 'Credit Card',
-		amount: '$12,003.00',
-		icon: <CreditCardIcon />,
-		color: '#347cc5', // Predefined MUI color
-	},
-];
+import { budgets } from '../../../../../constants/budgets';
 
 const BudgetItem = ({ title, amount, icon, color }) => {
 	return (
