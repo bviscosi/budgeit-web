7bc7d6cc59be952a70dc129db21dd710fff51da6
Ben <benviscosi@gmail.com>
2024-03-31 21:12:32 -0400

    remove unused imports

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import { AppBar, IconButton, Stack, useMediaQuery, useTheme } from '@mui/material';
-import React from 'react';
 import { navbarContent } from '../../utils/assets';
 import GetStartedButton from '../Buttons/GetStartedButton';
 import ThemeModeToggle from '../ThemeModeToggle/ThemeModeToggle';
