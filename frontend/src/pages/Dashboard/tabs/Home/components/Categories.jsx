2a276be50988ac68766d3341b80d56bc96038c38
Ben <benviscosi@gmail.com>
2024-05-18 15:01:33 -0400

    use transactions context for categories dougnut chart

--- frontend/src/pages/Dashboard/tabs/Home/components/Categories.jsx
+++ frontend/src/pages/Dashboard/tabs/Home/components/Categories.jsx
@@ -1,58 +1,41 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 
-import { addJwtHeader } from '../../../../../utils/addJwtHeader';
+import { Box, Card, CardContent, Typography } from '@mui/material';
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
-import { Box, Card, CardContent, Typography } from '@mui/material';
+
+import { useTransactions } from '../../../../../context/TransactionsContext';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Categories = () => {
 	const [categoryTotals, setCategoryTotals] = useState({});
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState('');
-
-	useEffect(() => {
-		const fetchTransactions = async (startDate, endDate) => {
-			setLoading(true);
-			setError('');
-			try {
-				const response = await axios.get(
-					`/transactions?startDate=${startDate}&endDate=${endDate}`,
-					{ headers: addJwtHeader() }
-				);
-				const transactions = response.data.transactions;
-
-				const totals = transactions.reduce((acc, transaction) => {
-					const category = transaction.category[0];
-					if (!acc[category]) {
-						acc[category] = 0;
-					}
-					acc[category] += parseFloat(transaction.amount); // Ensure the amount is parsed as a number
-					return acc;
-				}, {});
-
-				setCategoryTotals(totals);
-			} catch (error) {
-				console.error('Error fetching transactions:', error);
-				setError('Failed to fetch transactions');
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		const startDate = '2023-11-01';
-		const endDate = '2024-01-01';
-		fetchTransactions(startDate, endDate);
-	}, []);
+	const { transactions, loading, error } = useTransactions();
 
 	const data = {
-		labels: Object.keys(categoryTotals),
+		labels: Object.keys(
+			transactions.reduce((acc, transaction) => {
+				const category = transaction.category[0];
+				if (!acc[category]) {
+					acc[category] = 0;
+				}
+				acc[category] += parseFloat(transaction.amount); // Ensure the amount is parsed as a number
+				return acc;
+			}, {})
+		),
 		datasets: [
 			{
-				data: Object.values(categoryTotals),
+				data: Object.values(
+					transactions.reduce((acc, transaction) => {
+						const category = transaction.category[0];
+						if (!acc[category]) {
+							acc[category] = 0;
+						}
+						acc[category] += parseFloat(transaction.amount); // Ensure the amount is parsed as a number
+						return acc;
+					}, {})
+				),
 				backgroundColor: [
 					'#FF6384',
 					'#36A2EB',
