6d4dda3e2d7e5fd4e757931d59d21a91c4acfff2
Ben <benviscosi@gmail.com>
2024-05-18 14:59:34 -0400

    use transactions context on transactions page

--- frontend/src/pages/Dashboard/tabs/Transactions/Transactions.jsx
+++ frontend/src/pages/Dashboard/tabs/Transactions/Transactions.jsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
 import { DataGrid } from '@mui/x-data-grid';
-import { Card, Paper } from '@mui/material';
-import { addJwtHeader } from '../../../../utils/addJwtHeader';
+import { Card } from '@mui/material';
+import { useTransactions } from '../../../../context/TransactionsContext';
 
 const Transactions = () => {
-	// Define the columns for the DataGrid
 	const columns = [
 		{ field: 'date', headerName: 'Date', width: '300' },
 		{ field: 'merchant_name', headerName: 'Merchant', width: '300' },
@@ -19,44 +15,15 @@ const Transactions = () => {
 		},
 	];
 
-	const [rows, setRows] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState('');
-
-	useEffect(() => {
-		const fetchTransactions = async (startDate, endDate) => {
-			setLoading(true);
-			setError('');
-			try {
-				const response = await axios.get(
-					`/transactions?startDate=${startDate}&endDate=${endDate}`,
-					{ headers: addJwtHeader() }
-				);
-
-				const fetchedRows = response.data.transactions.map((transaction, index) => ({
-					id: index, // DataGrid requires a unique 'id' field for each row
-					...transaction,
-				}));
-
-				setRows(fetchedRows);
-			} catch (error) {
-				console.error('Error fetching transactions:', error);
-				setError('Failed to fetch transactions');
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		// Example dates, replace with actual dynamic dates
-		const startDate = '2023-11-01';
-		const endDate = '2024-01-01';
-		fetchTransactions(startDate, endDate);
-	}, []);
+	const { transactions, loading, error } = useTransactions();
 
 	return (
 		<Card sx={{ height: '100%', margin: '2rem', borderRadius: '1rem' }}>
 			<DataGrid
-				rows={rows}
+				rows={transactions.map((transaction, index) => ({
+					id: index, // DataGrid requires a unique 'id' field for each row
+					...transaction,
+				}))}
 				columns={columns}
 				pageSize={5}
 				rowsPerPageOptions={[5]}
